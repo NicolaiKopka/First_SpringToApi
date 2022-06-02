428fa0829845d3d61fc4559e7039a9f0a5fcea4e
Nicolai Kopka <nicolai.kopka@aol.de>
2022-06-02 15:39:28 +0200

    mock IT

--- src/main/java/io/github/nicolaikopka/firstspringtoapi/GitHubApi/GitRepo.java
+++ src/main/java/io/github/nicolaikopka/firstspringtoapi/GitHubApi/GitRepo.java
@@ -1,10 +1,13 @@
 package io.github.nicolaikopka.firstspringtoapi.GitHubApi;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class GitRepo {
 
     private String name;
--- src/test/java/io/github/nicolaikopka/firstspringtoapi/GitHubControllerIT.java
+++ src/test/java/io/github/nicolaikopka/firstspringtoapi/GitHubControllerIT.java
@@ -1,21 +1,28 @@
 package io.github.nicolaikopka.firstspringtoapi;
 
 import io.github.nicolaikopka.firstspringtoapi.GitHubApi.GitRepo;
+import io.github.nicolaikopka.firstspringtoapi.GitHubApi.GitRepoService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.Objects;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GitHubControllerIT {
 
     @Autowired
-    TestRestTemplate testRestTemplate;
+    private TestRestTemplate testRestTemplate;
+
+    @MockBean
+    private GitRepoService gitRepoService;
 
     @Test
     void caseGetListOfNames() {
@@ -24,6 +31,17 @@ class GitHubControllerIT {
         Assertions.assertThat(Objects.requireNonNull(response.getBody()).length).isEqualTo(19);
         Assertions.assertThat(response.getBody()[1].getName()).isEqualTo("Blog_Test");
         Assertions.assertThat(response.getBody()[2].getDescription()).isNull();
+    }
+
+    @Test
+    void caseGetListOfNameWithMock() {
+        GitRepo gitRepo1 = new GitRepo("repo1", "test");
+        GitRepo gitRepo2 = new GitRepo("repo2", "test2");
+        Mockito.when(gitRepoService.getRepo("NicolaiKopka")).thenReturn(List.of(gitRepo1, gitRepo2));
+        ResponseEntity<GitRepo[]> response = testRestTemplate.getForEntity("/github/NicolaiKopka", GitRepo[].class);
+        Assertions.assertThat(Objects.requireNonNull(response.getBody()).length).isEqualTo(2);
+        Assertions.assertThat(response.getBody()[0].getName()).isEqualTo("repo1");
+        Assertions.assertThat(response.getBody()[1].getDescription()).isEqualTo("test2");
     }
 
 }
